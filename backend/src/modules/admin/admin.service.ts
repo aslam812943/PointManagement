3d52d792c090fa761922239d6784d05ea1e77d22
Aslam <aslam812943@gmai.com>
2026-05-06 16:31:16 +0530

    fix:db url problem

--- backend/src/modules/admin/admin.service.ts
+++ backend/src/modules/admin/admin.service.ts
@@ -3,8 +3,20 @@ import { AdminRepository } from './admin.repository.js';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AdminService {
+export class AdminService{
   constructor(private readonly adminRepository: AdminRepository) {}
+
+  // async onModuleInit() {
+  //   const adminCount = (await this.adminRepository.findAll()).length;
+  //   if (adminCount === 0) {
+  //     const hashedPassword = await bcrypt.hash('admin123', 10);
+  //     await this.adminRepository.create({
+  //       username: 'admin',
+  //       password: hashedPassword,
+  //     });
+  //     console.log('Default admin user created: admin / admin123');
+  //   }
+  // }
 
   async findByUsername(username: string) {
     return this.adminRepository.findByUsername(username);
